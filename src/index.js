394f6b55ce2a5cd3534f9dbfcb447293178b94a6
marc-alzapiedi <marcalzapiedi@gmail.com>
2023-10-17 19:01:08 +0200

    imported cors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import app from "./server.js"
-const port = process.env.PORT || 4000
+import cors from "cors"
 
+
+const port = process.env.PORT || 4000
+app.use(cors())
 
 app.listen(port, () => {
     console.log(`\n Server is running on port ${port}\n`)
